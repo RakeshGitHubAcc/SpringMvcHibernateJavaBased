6758228001a74679fbef58a9ad07b50ed3694ee8
RakeshGitHubAcc <bishwal.rakeshkumar87@gmail.com>
2016-04-08 10:58:27 +0530

    First commit

---
+++ src/com/spring/dao/UserDAO.java
@@ -0,0 +1,12 @@
+package com.spring.dao;
+
+import java.util.List;
+
+import com.spring.model.User;
+
+public interface UserDAO {
+	public List<User> list();
+    public User get(int id);
+    public void saveOrUpdate(User user);
+    public void delete(int id);
+}
